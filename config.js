ca68ce7bc0839ab476fbfa25fa26dd1654092dfe
minhhoang205 <106818439+minhhoang205@users.noreply.github.com>
2022-06-07 22:24:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "withlove",
+    introTitle: 'withlove',
+    introDesc: `Cho t order 1 ly nước cam
+    tâm tình nguyện yêu em`,
+    btnIntro: 'here bae',
+    title: 'tớ đã quá thích m r, phải làm sao phải làm sao🥰',
+    desc: 'Cậu thấy vậy có được hok ',
+    btnYes: 'cx có thích m nà <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'làm thao để thành bạn trai m ây :((',
+    btnReply: 'send it bae',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Cuối cùng thì 😘😘',
+    messDesc: 'Làm NY t nhaaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/profile.php?id=100050931922709
 }
